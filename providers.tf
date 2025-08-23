70919b30929e35c8967d607b77e18ab3a66cc659
prodyut <prodyut@kloudgurdian.com>
2025-08-23 17:28:50 +0530

    provider

--- providers.tf
+++ providers.tf
@@ -1,19 +1,10 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name = "RG-Terraform-Storage-Account"
-    storage_account_name = "tfstorage43blrg"
-    container_name       = "tfstate"
-    key                  = "${var.vm_name}.tfstate"
-  }
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
   }
-
-  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
