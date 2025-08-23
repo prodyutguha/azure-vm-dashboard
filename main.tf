1840d0e9725f4c50715b402ed63bc656a0ac10b3
prodyut <prodyut@kloudgurdian.com>
2025-08-23 18:19:39 +0530

    update os version

--- main.tf
+++ main.tf
@@ -116,7 +116,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
+    offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
   }
