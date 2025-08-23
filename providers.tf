20e1891a79d1bd91f198a0efa37442e36a033ba7
prodyut <prodyut@kloudgurdian.com>
2025-08-23 16:31:55 +0530

    dsad

--- providers.tf
+++ providers.tf
@@ -2,17 +2,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.10.0"
-    }
-
-    random = {
-      source = "hashicorp/random"
-      version = "3.4.3"
-    }
-
-    tls = {
-      source = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = "~>3.0"
     }
   }
+  required_version = ">=1.3.0"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
--- variables.tf
+++ variables.tf
@@ -38,3 +38,8 @@ variable "ssh_public_key" {
   description = "Your SSH public key (ssh-rsa ...)"
   type        = string
 }
+
+variable "subscription_id" { type = string }
+variable "tenant_id" { type = string }
+variable "client_id" { type = string }
+variable "client_secret" { type = string }
