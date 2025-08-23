8940aa470316a3e10b0066cc087dded41bb95597
prodyut <prodyut@kloudgurdian.com>
2025-08-23 16:23:59 +0530

    testing

--- providers.tf
+++ providers.tf
@@ -1,13 +1,18 @@
 terraform {
-  required_version = ">= 1.5.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.100.0"
+      version = ">= 4.10.0"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version = "3.4.3"
+    }
+
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 4.0.4"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
 }
